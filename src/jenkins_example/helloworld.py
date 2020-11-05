524185baa56f8e5fb427058b30c71f452b9f1d50
David Hadley <davehadley@users.noreply.github.com>
2020-11-05 11:39:53 +0000

    Run black on all files.

--- src/jenkins_example/helloworld.py
+++ src/jenkins_example/helloworld.py
@@ -1,3 +1,2 @@
-
 def helloworld() -> str:
     return "Hello World"
--- tests/test_helloworld.py
+++ tests/test_helloworld.py
@@ -1,4 +1,5 @@
 from jenkins_example.helloworld import helloworld
 
+
 def test_helloworld():
     assert helloworld() == "Hello World"
